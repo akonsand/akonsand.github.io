664b520a0ee5e0ad899bd71da22385882b4e4049
akonsand <naved.merchant@gmail.com>
2015-12-23 22:10:20 -0500

    Deleted all, to make space for the site

--- js/main.js
+++
@@ -1,8 +0,0 @@
-var peer = new Peer({key: 'ytgklpf684u0udi'});
-
-var connection;
-peer.on('open',function(id){
-   console.log('The ID of this client is ' + id);
-   document.getElementById('peerIdDisplay').innerHTML = id;
-});
-
